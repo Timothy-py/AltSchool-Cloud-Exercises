db4ebc3f30eedfddc99330865fd24c81a4c2c7fb
Timothy-py <adeyeyetimothy33@gmail.com>
2022-09-29 23:58:35 +0100

    added attachment

--- Exercise8/mailer_script.sh
+++ Exercise8/mailer_script.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 FILE="/home/vagrant/memory_logs.txt"
-RECIPIENT=adeyeyetimothy33@gmail.com
+RECIPIENT="adeyeyetimothy33@gmail.com"
 SUBJECT="System memory logs"
-BODY >>cat $FILE
+BODY="The details of your memory usage can be found in the attached file."
 
-mail -s $SUBJECT $RECIPIENT >>$BODY
+mail -s "$SUBJECT" $RECIPIENT -A $FILE <<<"$BODY"
 
 rm -r $FILE
