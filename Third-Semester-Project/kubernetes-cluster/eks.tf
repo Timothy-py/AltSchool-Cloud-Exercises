93289993003e5d5fcb473b1b608adb1c4b901325
Timothy-py <adeyeyetimothy33@gmail.com>
2023-03-18 23:00:58 +0100

    setup nodes, autoscaler, oidc

---
+++ Third-Semester-Project/kubernetes-cluster/eks.tf
@@ -0,0 +1,39 @@
+resource "aws_iam_role" "exam3" {
+  name = "eks-cluster-exam3"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "exam3-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.exam3.name
+}
+
+resource "aws_eks_cluster" "exam3" {
+  name     = "exam3"
+  role_arn = aws_iam_role.exam3.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.exam3-AmazonEKSClusterPolicy]
+}
